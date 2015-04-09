58bfd179a06f5d7722a80ba3f7fa3c092d83c213
Ivan Kornilov <vano468@gmail.com>
2015-04-09 16:17:01 +0300

    Refactor relations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,7 @@
 class Department < ActiveRecord::Base
-  has_one :boss, class_name: 'User', foreign_key: 'id'
-  has_many :employees, class_name: 'User', foreign_key: 'department_id'
-  belongs_to :parent, class_name: 'Department', foreign_key: 'parent_id'
-  has_many :children, class_name: 'Department', foreign_key: 'parent_id'
+  belongs_to :parent,  class_name: 'Department', foreign_key: 'parent_id'
+  belongs_to :boss,    class_name: 'User',       foreign_key: 'boss_id'
+
+  has_many :employees, class_name: 'User',       foreign_key: 'department_id'
+  has_many :children,  class_name: 'Department', foreign_key: 'parent_id'
 end
