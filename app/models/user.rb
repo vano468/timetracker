2fbd15a58f2a3e54508aa4200f1dc6c6978d04c8
Ivan Kornilov <vano468@gmail.com>
2015-04-09 19:45:34 +0300

    Fix user boss_of relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
   belongs_to :department
-  belongs_to :boss_of, class_name: 'Department', foreign_key: 'boss_id'
+  has_one :boss_of, class_name: 'Department', foreign_key: 'boss_id'
+
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
