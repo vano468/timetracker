0abd84aaaf1c6a46820e72c0ee9acc345ebbd24a
Ivan Kornilov <vano468@gmail.com>
2015-04-22 13:14:52 +0300

    Implement ajax helper

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -1,14 +1,18 @@
 class Admin::DepartmentsController < ApplicationController
+  include AjaxHelper
 
   def new
     @department = Department.new
-    Rails.application.routes.recognize_path(request.referrer)[:id]
     respond_to do |format|
       format.js
     end
   end
 
   def create
+    Rails.application.routes.recognize_path(request.referrer)[:id]
+    respond_to do |format|
+      format.js { render ajax_redirect_to(departments_path) }
+    end
   end
 
   def edit
