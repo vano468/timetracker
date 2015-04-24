6a72895fc958f3076963f944e0a6dd92fadc7462
Murad Yusufov <flood4life@gmail.com>
2015-04-24 15:19:25 +0300

    Add hh:mm format validation for Worktime

--- app/models/records/worktime.rb
+++ app/models/records/worktime.rb
@@ -1,10 +1,10 @@
 class Worktime < Record
   attr_accessor :time_from, :time_to, :day
 
-  validates_time :time_from
-  validates_time :time_to
+  validates_time :time_from, :time_to
+  validates      :time_from, :time_to, format: { with: /\A[0-2][0-9]:[0-5][0-9]\z/, message: 'should be in hh:mm format' }
 
-  before_validation :build_record
+  before_save :build_record
 
   scope :user, -> (user) { where user: user }
 
@@ -21,8 +21,8 @@ private
   end
 
   def date_from_lesser_than_date_to
-    if date_from >= date_to
-      errors.add :time_to, 'can\'t be before or equal date_from'
+    if time_from >= time_to
+      errors.add :time_to, 'can\'t be before or equal From Time'
     end
   end
 end
