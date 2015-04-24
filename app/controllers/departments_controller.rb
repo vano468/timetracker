2c0c8fd5007ec93c440ca307a57d4b540cd4b94f
Ivan Kornilov <vano468@gmail.com>
2015-04-24 14:52:06 +0300

    Remove separte departments#index page

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,10 +1,11 @@
 class DepartmentsController < ApplicationController
   before_action :set_department,  only: :show
-  before_action :set_breadcrumbs, only: :show
+  before_action :set_breadcrumbs, only: [:index, :show]
 
   def index
     @departments = Department.top_level
     @users = User.without_department
+    render :show
   end
 
   def show
@@ -23,8 +24,10 @@ private
 
   def set_breadcrumbs
     add_breadcrumb 'Root', departments_path
-    @department.hierarchy.each do |dep|
-      add_breadcrumb dep.title, department_path(dep)
+    if @department
+      @department.hierarchy.each do |dep|
+        add_breadcrumb dep.title, department_path(dep)
+      end
     end
   end
 end
