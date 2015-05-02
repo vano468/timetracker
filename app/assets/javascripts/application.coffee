8c65af49ae560b2f45a458b533bab01783cfae14
Ivan Kornilov <vano468@gmail.com>
2015-05-02 23:15:01 +0300

    Implement record form design, fix record validations

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -1,6 +1,10 @@
 #= require jquery
 #= require jquery_ujs
 #= require bootstrap
+#= require moment
+#= require bootstrap-datetimepicker
+#= require pickers
+
 #= require_self
 #= require_tree ./components
 #= require_tree ./pages
--- app/models/record.rb
+++ app/models/record.rb
@@ -6,8 +6,9 @@ class Record < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validate :date_from_lesser_than_date_to, unless: -> (record) { record.is_a? Worktime }
-  validate :dates_cannot_be_in_the_past, :emails_should_be_valid, unless: -> (record) { record.is_a? Worktime }
+  validates :date_from, :date_to, date: true
+  validate :dates_cannot_be_in_the_past, :date_from_lesser_than_date_to, :emails_should_be_valid,
+           unless: -> (record) { record.is_a?(Worktime) || !record.errors.blank? }
 
   after_save :send_notifications
 
@@ -17,18 +18,18 @@ private
     puts "Let's send some notifications for those emails: #{emails}"
   end
 
-  def date_from_lesser_than_date_to
-    if date_from > date_to
-      errors.add :date_to, 'can\'t be before date_from'
+  def dates_cannot_be_in_the_past
+    if date_from < Date.today
+      errors.add :date_from, 'can\'t be in the past'
+    end
+    if date_to < Date.today
+      errors.add :date_to, 'can\'t be in the past'
     end
   end
 
-  def dates_cannot_be_in_the_past
-    if date_from.day < Date.today.day
-      errors.add :date_from, 'can\'t be in the past'
-    end
-    if date_to.day < Date.today.day
-      errors.add :date_to, 'can\'t be in the past'
+  def date_from_lesser_than_date_to
+    if date_from > date_to
+      errors.add :date_to, 'can\'t be before date_from'
     end
   end
 
