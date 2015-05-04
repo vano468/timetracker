e793259a8a2e5ac9734f6a807a3333f091b71bea
Murad Yusufov <flood4life@gmail.com>
2015-05-04 13:42:25 +0300

    Fix display of worktime in calendar

--- app/queries/work_status_for_employee_on_day.rb
+++ app/queries/work_status_for_employee_on_day.rb
@@ -3,7 +3,7 @@ class WorkStatusForEmployeeOnDay
     single_day_record = SingleDayRecordsForEmployeeOnDay.result(employee, day).take
     multi_day_record  = MultiDayRecordsForEmployeeOnDay.result(employee, day).take
 
-    return 0                                 if single_day_record.nil? and multi_day_record.nil?
+    return { hours: 0, minutes: 0 }          if single_day_record.nil? and multi_day_record.nil?
     return work_hours_for_day(employee, day) if single_day_record.is_a? Worktime
     return single_day_record.type            if single_day_record.present?
     return multi_day_record.type             if multi_day_record.present?
@@ -15,10 +15,13 @@ class WorkStatusForEmployeeOnDay
 private
 
   def self.work_hours_for_day(employee, day)
-    result = 0
+    result = { hours: 0, minutes: 0 }
+    total_seconds = 0
     SingleDayRecordsForEmployeeOnDay.result(employee, day).each do |record|
-      result += record.date_to - record.date_from
+      total_seconds += record.date_to - record.date_from
     end
-    result / (60 * 60) # datetime stamps are in seconds, method returns hours
+    result[:hours] = total_seconds.div(60 * 60)
+    result[:minutes] = total_seconds.modulo(60 * 60).div(60)
+    result
   end
 end
