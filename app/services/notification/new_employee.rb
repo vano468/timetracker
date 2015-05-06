b1d6e0612ac6c984d07a767e12aaa120d66e3607
Ivan Kornilov <vano468@gmail.com>
2015-05-07 00:04:35 +0300

    Send password after user creating

--- app/services/notification/new_employee.rb
+++ app/services/notification/new_employee.rb
@@ -1,7 +1,7 @@
 module Notification
   class NewEmployee
     def after_create_user(user)
-      UserMailer.send_password(user, 'password').deliver
+      UserMailer.send_password(user, user.password).deliver
     end
   end
 end
