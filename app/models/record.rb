784986f344bedf1555eef5187a8b3a6f290c27cc
Ivan Kornilov <vano468@gmail.com>
2015-05-06 18:57:45 +0300

    Fix record validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,29 +6,32 @@ class Record < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :date_from, :date_to, date: true, unless: -> (record) { record.is_a?(Worktime) }
-  validate :dates_cannot_be_in_the_past, :date_from_lesser_than_date_to, :emails_should_be_valid, :records_should_not_overlap,
-           unless: -> (record) { record.is_a?(Worktime) || !record.errors.blank? }
+  validates :date_from, :date_to, date: true, unless: :is_record_worktime?
+  validate :dates_cannot_be_in_the_past, :date_from_lesser_than_date_to, :emails_should_be_valid, :records_should_not_overlap, unless: :is_record_worktime?
 
   after_save :send_notifications
 
 private
+
+  def is_record_worktime?
+    self.is_a? Worktime
+  end
 
   def send_notifications
     puts "Let's send some notifications for those emails: #{emails}"
   end
 
   def dates_cannot_be_in_the_past
-    if date_from < Date.today
+    if date_from && date_from < Date.today
       errors.add :date_from, 'can\'t be in the past'
     end
-    if date_to < Date.today
+    if date_to && date_to < Date.today
       errors.add :date_to, 'can\'t be in the past'
     end
   end
 
   def date_from_lesser_than_date_to
-    if date_from > date_to
+    if date_from && date_to && date_from > date_to
       errors.add :date_to, 'can\'t be before date_from'
     end
   end
@@ -36,7 +39,7 @@ private
   def emails_should_be_valid
     emails_array = emails.split
     emails_array.each_with_index do |email, index|
-      errors.add("email_#{index}", "#{email} is invalid") unless ValidateEmail.valid? email
+      errors.add(:emails, "#{email} is invalid") unless ValidateEmail.valid? email
     end
   end
 
