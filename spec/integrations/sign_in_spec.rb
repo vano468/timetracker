d354dddfd9e3deef3ef6598150f882e3b0a71572
Alexander Shelyugov <shelyugov@isolat.co>
2015-05-21 00:15:25 +0300

    Fix and refactor the sign in test, add id to the submit button at the sign in page

--- spec/integrations/sign_in_spec.rb
+++ spec/integrations/sign_in_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe Devise, type: :view do
   describe 'the sign in process', type: :feature do
     let(:user) { FactoryGirl.create :user }
 
-    it 'should login successfully' do
+    it 'passes authentication' do
       visit '/users/sign_in'
       within '#new_user' do
         fill_in('email', with: user.email)
         fill_in('password', with: user.password)
       end
-      click_button 'login'
-      expect(page).to have_content user.email
+      click_button('login')
+      expect(page).to have_content(user.email)
+      expect(page).not_to have_button('login')
     end
   end
 end
